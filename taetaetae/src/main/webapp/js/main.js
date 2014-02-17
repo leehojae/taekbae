c40f094ba5e1987b032e9f2ea991f296723eb1d4
kimjinmin <777loveme@esants.co.kr>
2014-02-16 21:54:19 -0800

    excel upload

---
+++ taetaetae/src/main/webapp/js/main.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+    
+    listExcel();
+};
+function listExcel() {
+    $.ajax('excel/ajax/list.do', {
+            method: 'GET', 
+            success: function(excels){
+                var m = null;
+                var tr = null;
+                var html = null;
+                var table = $('listTable');
+                for (var i = 0; i < excels.length; i++) {
+                    e = excels[i];
+                    tr = document.createElement('tr');
+                    html = '';
+                    html += '<td>' + e.num + '</td>'; 
+                    html += '<td><a href="#" class="excelName" data-no="' + 
+                        e.num + '">' + 
+                        e.sender + '</a></td>';   
+                    html += '<td>' + e.receiver + '</td>'; 
+                    html += '<td>' + e.date + '</td>';   
+                  tr.innerHTML = html;
+                  table.appendChild(tr);
+                }
+                
+                var alist = document.querySelectorAll('.excelName');
+                for (var i = 0; i < alist.length; i++) {
+                    alist[i].onclick = function() {
+                        loadExcel(this.getAttribute("data-no"));
+                    };
+                }
+            }, 
+            error: function(){
+                alert('시스템이 바쁩니다.\n나중에 다시 시도해 주세요!');
+    }});
+}
+
+function loadExcel(no) {
+    $.ajax('ajax/read.do?no=' + no, {
+            method: 'GET', 
+            success: function(excel){
+        $("eNum").value = excel.num;
+        $("eSender").value = excel.sender;
+        $("eReceiver").value = excel.receiver;
+        $("eDate").value = excel.date;
+    }});
+}
