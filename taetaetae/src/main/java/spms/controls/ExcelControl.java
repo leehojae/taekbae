18804fee90b0d6d67f5ae2783c4bb90f6d7bfe76
bit <bit@localhost>
2014-02-18 08:36:27 +0900

    .

--- taetaetae/src/main/java/spms/controls/ExcelControl.java
+++ taetaetae/src/main/java/spms/controls/ExcelControl.java
@@ -2,36 +2,26 @@ package spms.controls;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import spms.dao.ExcelDao;
 import spms.vo.Excel;
@@ -47,6 +37,12 @@ public class ExcelControl {
 	
 	@Autowired(required=false)
 	ExcelDao excelDao;
+	
+	@RequestMapping("/delete")
+	public String delete(Model model) throws Exception {
+		excelDao.delete();
+		return "redirect:../main.do";
+	}
 	
 	@RequestMapping("/list")
 	public String list(Model model) throws Exception {
@@ -77,7 +73,7 @@ public class ExcelControl {
 
 	@RequestMapping(value="/add",method=RequestMethod.POST)
 	
-	public String processForm(@RequestParam("excelFile") MultipartFile excelFile, Excel excel, BindingResult result) throws IOException{
+	public String processForm(@RequestParam("excelFile") MultipartFile excelFile, Excel excel, BindingResult result) throws Exception{
 		if(!result.hasErrors()){
 			//list.removeAll(list);
 			list.clear();
