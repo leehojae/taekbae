ec7eb8b63612dcdea240e2354211b12f46ef8636
donghokwak <goodddong@gmail.com>
2014-03-28 20:58:42 +0900

    dh

--- taetaetae/src/main/webapp/js/excelUpload.js
+++ taetaetae/src/main/webapp/js/excelUpload.js
@@ -1,8 +1,8 @@
 //var fileInput = document.getElementById("excelFile");
 //var file = fileInput.files[0];
-//var formData = new FormData();
-//formData.append("excelFile", file);
-//
-//var xhr = new XMLHttpRequest();
-//xhr.open("POST", "excel/add.do", true);
-//xhr.send(formData);
+var formData = new FormData();
+formData.append("excelFile", file);
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "excel/add.do", true);
+xhr.send(formData);
