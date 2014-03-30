a5c436632ac8123508fa1d76fdf694d0c71f5384
kimjinmin <777loveme@esants.co.kr>
2014-03-30 21:00:59 +0900

    멤버 등록

--- taetaetae/src/main/java/spms/dao/MemberDao.java
+++ taetaetae/src/main/java/spms/dao/MemberDao.java
@@ -8,6 +8,7 @@ import spms.vo.Member;
 public interface MemberDao {
 	public List<Member> selectList(int no) throws Exception;
 	public Member selectOne(int no) throws Exception;
+	public Member searchid(String id) throws Exception;
 	public int insert(Member member) throws Exception;
 	public int update(Member member) throws Exception;
 	public int delete(int no) throws Exception;
